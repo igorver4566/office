39dd1fa2a739e8240bb5ba7690d473aec91ea226
Игорь Вересов <igorveresov@MacBook-Air-Igor.local>
2018-10-02 19:54:26 +0300

    update

--- public/view/src/App.vue
+++ public/view/src/App.vue
@@ -1,16 +1,41 @@
 <template>
   <v-app>
-  <v-navigation-drawer app></v-navigation-drawer>
-  <v-toolbar app></v-toolbar>
+  <v-navigation-drawer 
+    app
+    v-model="drawer"
+  ></v-navigation-drawer>
+  <v-toolbar app>
+      <v-toolbar-side-icon
+        @click="drawer = !drawer"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>SoftSetters</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn 
+          v-for="link in links"
+          :key="link.title"
+          :to="link.url"
+          flat
+        >
+        <v-icon>{{link.title}}</v-icon></v-btn>
+      </v-toolbar-items>
+  </v-toolbar>
   <v-content>
-    <v-container fluid>
       <router-view></router-view>
-    </v-container>
   </v-content>
-  <v-footer app></v-footer>
 </v-app>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      drawer: false,
+      links: [
+        {title: 'exit_to_app', url: '/login'},
+        {title: 'face', url: '/register'}
+      ]
+    }
+  }
+}
 </script>
--- public/view/src/router/index.js
+++ public/view/src/router/index.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/components/Home'
+import Login from '@/components/Auth/Login'
+import Register from '@/components/Auth/Register'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [
+    {
+      path: '',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    }
+  ]
 })
