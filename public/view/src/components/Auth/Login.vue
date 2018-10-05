ad3c7566d57f83a9dc521ae1524b1f2083152d96
Игорь Вересов <igorveresov@MacBook-Air-Igor.local>
2018-10-05 17:52:14 +0300

    Login and Reister

--- public/view/src/components/Auth/Login.vue
+++ public/view/src/components/Auth/Login.vue
@@ -1,17 +1,73 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Login</h1>
-            </v-flex>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md5>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Вход</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form ref="form" v-model="valid" validation>
+                  <v-text-field 
+                    prepend-icon="person" 
+                    name="login" 
+                    :rules="loginRules"
+                    label="Логин" 
+                    type="text" 
+                    v-model="login" 
+                    required
+                  ></v-text-field>
+                  <v-text-field 
+                    prepend-icon="lock" 
+                    name="password" 
+                    :rules="passRules"
+                    label="Пароль" 
+                    type="password" 
+                    v-model="password" 
+                    required
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn 
+                  color="primary" 
+                  @click="onSubmit"
+                  :disabled="!valid"
+                >Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
         </v-layout>
-    </v-container>
+      </v-container>
 </template>
 
 <script>
-export default {
-    data (){
-        return{}
+  export default {
+    data () {
+      return {
+        login: '',
+        password: '',
+        valid: false,
+        loginRules: [
+          v => !!v || 'Login is required'
+        ],
+        passRules: [
+          v => !!v || 'Password is required',
+          v => (v && v.length) >= 6 || 'Password must be equal or more than 6 characters'
+        ]
+      }
+    },
+    methods: {
+      onSubmit () {
+        if (this.$refs.form.validate()) {
+          const user = {
+            login: this.login,
+            password: this.password
+          }
+          console.log(user)
+        }
+      }
     }
-}
+  }
 </script>
--- public/view/src/components/Auth/Register.vue
+++ public/view/src/components/Auth/Register.vue
@@ -1,17 +1,87 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Reg</h1>
-            </v-flex>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md5>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Вход</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form ref="form" v-model="valid" validation>
+                  <v-text-field 
+                    prepend-icon="person" 
+                    name="login" 
+                    :rules="loginRules"
+                    label="Логин" 
+                    type="text" 
+                    v-model="login" 
+                    required
+                  ></v-text-field>
+                  <v-text-field 
+                    prepend-icon="lock" 
+                    name="password" 
+                    :rules="passRules"
+                    label="Пароль" 
+                    type="password" 
+                    v-model="password" 
+                    required
+                  ></v-text-field>
+                  <v-text-field 
+                    id="password" 
+                    prepend-icon="lock" 
+                    name="confirmPassword" 
+                    :rules="confirmPasswordRules"
+                    label="Подтвердите пароль" 
+                    type="password" 
+                    v-model="confirmPassword" 
+                    required
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn 
+                  color="primary" 
+                  @click="onSubmit"
+                  :disabled="!valid"
+                >Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
         </v-layout>
-    </v-container>
+      </v-container>
 </template>
 
 <script>
-export default {
-    data (){
-        return{}
+  export default {
+    data () {
+      return {
+        login: '',
+        password: '',
+        valid: false,
+        loginRules: [
+          v => !!v || 'Login is required'
+        ],
+        passRules: [
+          v => !!v || 'Password is required',
+          v => (v && v.length) >= 6 || 'Пароль должен иметь больше 6 символов'
+        ],
+        confirmPasswordRules: [
+          v => !!v || 'Password is required',
+          v => v === this.password || 'Пароли должны совпадать'
+        ]
+      }
+    },
+    methods: {
+      onSubmit () {
+        if (this.$refs.form.validate()) {
+          const user = {
+            login: this.login,
+            password: this.password
+          }
+          console.log(user)
+        }
+      }
     }
-}
+  }
 </script>
