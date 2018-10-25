3aff75cba72d4469dac04367eb4235bc8cac94e9
Игорь Вересов <igorveresov@MacBook-Air-Igor.local>
2018-10-25 12:26:26 +0300

    New model for task and rewrite query to db

--- src/system/app/db/task.go
+++ src/system/app/db/task.go
@@ -21,7 +21,24 @@ type Task struct {
 	MakeSlack   int8   `json:"make_slack"`
 	Message     string `json:"message"`
 	NameSlack   string `json:"name_slack"`
-	StatusID    string `json:"status_id"`
+	StatusID    int    `json:"status_id"`
+}
+
+//Task model for return to client
+type TaskReturn struct {
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	Access     string `json:"access"`
+	Manager    string `json:"manager"`
+	TimeDev    int    `json:"time_dev"`
+	TimeManage int    `json:"time_manage"`
+	Owner      string `json:"owner"`
+	Developer  string `json:"developer"`
+	Price      int    `json:"price"`
+	Tags       string `json:"tags"`
+	Message    string `json:"message"`
+	NameSlack  string `json:"name_slack"`
+	Status     string `json:"status"`
 }
 
 //NewTask - make new task. Return json {ok: var1, data: var2}
@@ -55,19 +72,19 @@ func (task *Task) NewTask() []byte {
 	return str
 }
 
-func GetAllTasks() ([]Task, error) {
-	var tasksArr []Task
-	var task Task
+func GetAllTasks() ([]TaskReturn, error) {
+	var tasksArr []TaskReturn
+	var task TaskReturn
 	d := Init()
 	defer d.Close()
-	rows, err := d.Query("SELECT * FROM task ORDER BY id")
+	rows, err := d.Query("SELECT t.id, t.name, t.access, u.name, t.time_dev, t.time_manage, o.name, u2.name, t.price, t.tags, t.message, t.name_slack, s.name FROM task t INNER JOIN users u ON t.manager_id = u.id INNER JOIN owner o ON t.owner_id = o.id INNER JOIN users u2 ON t.developer_id = u2.id INNER JOIN status s ON t.status_id = s.id ORDER BY t.id")
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		err := rows.Scan(&task.ID, &task.Name, &task.Access, &task.ManagerID, &task.TimeDev, &task.TimeManage, &task.OwnerID, &task.DeveloperID, &task.Price, &task.Tags, &task.MakeSlack, &task.Message, &task.NameSlack, &task.StatusID)
+		err := rows.Scan(&task.ID, &task.Name, &task.Access, &task.Manager, &task.TimeDev, &task.TimeManage, &task.Owner, &task.Developer, &task.Price, &task.Tags, &task.Message, &task.NameSlack, &task.Status)
 		if err != nil {
 			return nil, err
 		}
