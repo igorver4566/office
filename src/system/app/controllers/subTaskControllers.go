e747108c6b8bbf7f5bc2b2fae0bc2b45f57ea73d
Игорь Вересов <igorveresov@MacBook-Air-Igor.local>
2018-10-25 14:18:28 +0300

    Make sub task on server-side

---
+++ src/system/app/controllers/subTaskControllers.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"../auth"
+	"../db"
+)
+
+func MakeSubTask(w http.ResponseWriter, r *http.Request) {
+	var subTask db.SubTask
+	err := json.NewDecoder(r.Body).Decode(&subTask)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	if err != nil {
+		w.Write(auth.JsonResponseByVar("false", err.Error()))
+		panic(err)
+	}
+	res := subTask.NewSubTask()
+	w.Write(res)
+}
+
+func GetSubTasks(w http.ResponseWriter, r *http.Request) {
+	res, err := db.GetAllSubTasks()
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	if err != nil {
+		w.Write(auth.JsonResponseByVar("false", err.Error()))
+	}
+	w.Write(auth.JsonResponseByVar("true", res))
+}
---
+++ src/system/app/db/subTask.go
@@ -0,0 +1,80 @@
+package db
+
+import (
+	"strconv"
+
+	"../auth"
+)
+
+//SubTask model
+type SubTask struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Price    int    `json:"price"`
+	Time     int    `json:"time"`
+	Message  string `json:"message"`
+	TaskID   int    `json:"task_id"`
+	StatusID int    `json:"status_id"`
+	UserID   int    `json:"user_id"`
+}
+
+//SubTaskReturn model for return to client
+type SubTaskReturn struct {
+	ID      int    `json:"id"`
+	Name    string `json:"name"`
+	Price   int    `json:"price"`
+	Time    int    `json:"time"`
+	Message string `json:"message"`
+	TaskID  int    `json:"task_id"`
+	Status  string `json:"status"`
+	User    string `json:"user"`
+}
+
+//NewSubTask - make new subtask. Return json {ok: var1, data: var2}
+func (subTask *SubTask) NewSubTask() []byte {
+	var str []byte
+	d := Init()
+	defer d.Close()
+	insert, err := d.Prepare("INSERT INTO sub_task VALUES (NULL, ?, ?, ?, ?, ?, 1, ?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer insert.Close()
+	lastId, err := insert.Exec(subTask.Name,
+		subTask.Price,
+		subTask.Time,
+		subTask.Message,
+		subTask.TaskID,
+		subTask.UserID)
+	if err != nil {
+		str = auth.JsonResponseByVar("false", "Ошибка при создании задачи"+err.Error())
+	} else {
+		id, _ := lastId.LastInsertId()
+		str = auth.JsonResponseByVar("true", "Подзадача успешно создана под номером "+strconv.FormatInt(id, 10))
+	}
+	return str
+}
+
+func GetAllSubTasks() ([]SubTaskReturn, error) {
+	var subTaskArr []SubTaskReturn
+	var subTask SubTaskReturn
+	d := Init()
+	defer d.Close()
+	rows, err := d.Query("SELECT s.id, s.name, s.price, s.time, s.message, s.task_id, st.name, u.name FROM sub_task s INNER JOIN status st ON s.status_id = st.id INNER JOIN users u ON s.user_id = u.id ORDER BY s.id")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&subTask.ID, &subTask.Name, &subTask.Price, &subTask.Time, &subTask.Message, &subTask.TaskID, &subTask.Status, &subTask.User)
+		if err != nil {
+			return nil, err
+		}
+		subTaskArr = append(subTaskArr, subTask)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return subTaskArr, nil
+}
